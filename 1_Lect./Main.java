5d3769e22e9c39fdf98ff37139337dfd93ceee2e
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-08-16 16:27:50 +0530

    Demo

--- 1_Lect./Main.java
+++ 1_Lect./Main.java
@@ -1,5 +1,20 @@
+ 
+ import java.util.Scanner ;
 public class Main{
     public static void main(String[] args){
-        System.out.print("Hello world");
+        /* System.out.print("enter anything : "); */
+         Scanner input = new Scanner(System.in); 
+
+
+    //  System.out.println(input.next()) ;  ek hi word print hoyega or niche wale me pura sentence.
+
+
+       // System.out.println(input.nextLine()) ; 
+   
+       /*  String name = input.next();
+         System.out.println(name); */
+
+         float marks = input.nextFloat();
+         System.out.println(marks);
     }
 }
