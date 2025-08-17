beb93bf1634c163834b214f5cb0785643071daab
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-08-17 11:51:09 +0530

    notes

---
+++ 1_Lect./TypeCasting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TypeCasting{
+
+
+    // for converting float into integer
+    public  static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = (int)(67.987f);
+        System.out.println(num);
+    }
+}
