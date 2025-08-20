bd1ed39a3a5e7141d0ad926c784c7965e6b1c762
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-08-20 17:04:14 +0530

    notes

---
+++ 2_lec_Array/Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Array{
+    public static void main(String[] args){
+        //  String[] arr = new String[4];
+        //  System.out.println(arr[0]);
+        int[] arr = new int[4];
+ Scanner in = new Scanner(System.in);
+         arr[0]=90;
+         arr[1]=80;
+         arr[2]=66;
+         arr[3] = 90;
+         for(int i = 0; i < arr.length ; i++){
+            System.out.print("Enter number " + (i + 1) + ": ");
+           arr[i] =  in.nextInt();
+         }
+         // Entered
+         for(int i =0; i < arr.length ; i++){
+         System.out.print(arr[i] + " ");
+         }
+    }
+}
