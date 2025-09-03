6029d8ab67d2d307b829e1aaf63a878fa7cd7ab1
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-09-03 17:18:22 +0530

    notes

--- 5_BubbleSort/BubbleSort.java
+++ 5_BubbleSort/BubbleSort.java
@@ -1,12 +1,38 @@
 import java.util.Arrays;
+ 
 
+ // there are two method of sorting ..
+ 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] a = {5, 4, 3, 2, 1};
-        bubble(a);
+        int[] a = {50,45,32,66,78,90};
+        // bubble(a);
+        selection(a) ;
         System.out.println(Arrays.toString(a));
     }
 
+static void selection(int[] a){
+    for(int i=0; i<a.length ; i++){
+        // find the max item in the remaining arry and swap the current index 
+        int last = a.length - i -1;
+        int maxIndex = getMaxIndex(a, 0 ,  last) ;
+        swap(a, maxIndex , last);
+    }
+}
+ static void swap(int[] a , int first , int second){
+    int temp = a[first] ;
+    a[first] = a[second];
+    a[second]=temp;
+ }
+private static int getMaxIndex(int[] a , int start , int end ){
+    int max = start ; 
+for(int i= start ; i<= end ; i++){
+    if (a[max]  < a[i]){
+        max = i ;
+    }
+}    
+     return max ;
+}
     static void bubble(int[] a) {
         boolean swapped;
         // run steps in n-1
