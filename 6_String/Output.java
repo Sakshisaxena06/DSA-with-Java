908f516d4a2f413b526dc3dad86f285d08cdfa00
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-09-08 19:06:35 +0530

    notes

---
+++ 6_String/Output.java
@@ -0,0 +1,10 @@
+public class Output{
+    public static void main(String[] args){
+        // Integer num = new Integer(65);
+        // System.out.println(num.toString());  
+        float a = 90.12345f;
+        System.out.printf("Formated no . is %.2f " , a);   // it  round off the value 
+          System.out.printf("Pie : %.3f" , Math.PI);
+  System.out.printf(" helllo my name is %s, and i am %s" ,"Sakshi" , "smart" );
+  
+    }}
