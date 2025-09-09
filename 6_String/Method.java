bf7a96d24f0c7b1303635e801bec3c0a6ac9cb3e
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-09-09 15:57:53 +0530

    notes

---
+++ 6_String/Method.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+
+public class Method{
+    public static void main(String[] args){
+        String name = "Sakshi Saxena" ;
+        System.out.println(Arrays.toString(name.toCharArray())) ;
+         System.out.println(name.indexOf('a')) ;
+ System.out.println(Arrays.toString(name.split(" "))) ; // if we have space so it take as a new word starting 
+    }
+}
