b8bd994aaa15330cd38acf792b4d3f917aa48d10
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-09-09 15:28:51 +0530

    notes

---
+++ 6_String/Operator.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList ; 
+public class Operator{
+    public static void main(String[] args){
+        System.out.println('a'+'b');
+  System.out.println("a"+ "b");
+    System.out.println('a'+3);
+      System.out.println((char)('a'+'3'));
+  System.out.println("a"+ 1);
+
+  System.out.println("Kunal" + new ArrayList<>());
+    System.out.println("sakshi" + new Integer(56));
+    String ans = new Integer(90) + "" + new ArrayList<>() ;
+    System.out.println(ans);
+
+
+
+    }
+}
